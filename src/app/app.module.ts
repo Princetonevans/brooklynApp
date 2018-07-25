7411db8748d528fa57780ad6a0c98decbf072607
Princeton Evans <princetonevans@yahoo.com>
2018-07-25 17:32:03 -0500

    added more components and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { BlogComponent } from './blog/blog.component';
 import { AboutComponent } from './about/about.component';
 import { BlogShowComponent } from './blog-show/blog-show.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  { path: 'blog', component: BlogComponent },
+  { path: 'blog/:id', component: BlogShowComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
@@ -13,10 +25,15 @@ import { BlogShowComponent } from './blog-show/blog-show.component';
     NavBarComponent,
     BlogComponent,
     AboutComponent,
-    BlogShowComponent
+    BlogShowComponent,
+    PageNotFoundComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
